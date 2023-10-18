74577e16eb5bdcecfb8aad17ece1dae22b418cae
Paulhakeem <paulnyamawi18@gmail.com>
2023-10-18 18:53:39 +0300

    keep user logged in

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import { auth } from '../firebase'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -35,8 +35,22 @@ const router = createRouter({
   ]
 })
 
-router.beforeEach((to, from, next) => {
-  const isAuthenticated = auth.currentUser
+
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const removeListener = onAuthStateChanged(
+      getAuth(),
+      (user) => {
+        removeListener(),
+        resolve(user)
+      },
+      reject
+    )
+  })
+}
+
+router.beforeEach(async(to, from, next) => {
+  const isAuthenticated = await getCurrentUser()
   const isAuthRequired = to.matched.some(record => 
       record.meta.requiresAuth )
 
