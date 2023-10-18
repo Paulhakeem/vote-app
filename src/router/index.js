076f85e59522be9c2f58adddd93fdf6d96c3e29d
Paulhakeem <paulnyamawi18@gmail.com>
2023-10-19 00:10:58 +0300

    add firestore

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
-
+import { auth } from '../firebase'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
--- src/store/vote.js
+++ src/store/vote.js
@@ -1,7 +1,8 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
-import {RouterLink, useRouter} from 'vue-router'
-// import {db} from '../firebase.js'
+import { onMounted, ref } from 'vue'
+import { useRouter} from 'vue-router'
+ import {db} from '../firebase.js'
+ import { collection, addDoc, onSnapshot } from "firebase/firestore"
 
 const router = useRouter()
 
@@ -15,14 +16,13 @@ export const useVoteStore = defineStore('votes', () => {
     const newQuestion = ref([])
 
     const createQuestion = () => {
-       newQuestion.value.push({
-        id: Math.floor(Math.random() * 1000),
+        addDoc(collection(db, "vDetails"), {
         question: question.value,
         firstAnswer: firstAnswer.value,
         secondAnswer: secondAnswer.value,
         thirdAnswer: thirdAnswer.value,
         fourthAnswer: fourthAnswer.value,
-       })
+    })
        question.value = ''
        firstAnswer.value = ''
        secondAnswer.value = ''
@@ -30,6 +30,30 @@ export const useVoteStore = defineStore('votes', () => {
        fourthAnswer.value = ''
     }
 
+
+
+    onMounted(() => {
+      try {
+        onSnapshot(collection(db, "vDetails"), (querySnapshot) => {
+            const getVotes = [];
+            querySnapshot.forEach((doc) => {
+                const fetchVotes = {
+                    id: doc.id,
+                    question: doc.data().question,
+                    firstAnswer: doc.data().firstAnswer,
+                    secondAnswer: doc.data().secondAnswer,
+                    thirdAnswer: doc.data().thirdAnswer,
+                    fourthAnswer: doc.data().fourthAnswer,
+                }
+                getVotes.push(fetchVotes)
+            })
+            newQuestion.value = getVotes
+          })
+      }
+        catch (err) {
+
+        }
+    })
 
     return { question, firstAnswer, secondAnswer, thirdAnswer, fourthAnswer, createQuestion, newQuestion}
 })
